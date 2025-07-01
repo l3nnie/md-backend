9317440edd2b53576135bdaed69f052ecbf25ff7
l3nnie <lennymaina214@gmail.com>
2025-07-01 16:20:49 +0300

    Update server.js

--- server.js
+++ server.js
@@ -9,6 +9,12 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+// Root route fix
+app.get('/', (req, res) => {
+  res.send('API is running. Try /api/medical-education');
+});
+
+// API routes
 app.use('/api/medical-education', educationRoutes);
 
 app.listen(PORT, () => {

9dff9ea914a05b70a47083b1825817d6ce9f4979
l3nnie <lennymaina214@gmail.com>
2025-07-01 16:31:11 +0300

    Update gemini.config.js

--- config/gemini.config.js
+++ config/gemini.config.js
@@ -1,3 +1,3 @@
 const { GoogleGenerativeAI } = require('@google/generative-ai');
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
-module.exports = genAI.getGenerativeModel({ model: "Gemini 1.5 Flash" });
+module.exports = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
